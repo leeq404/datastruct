fe494f21dc62ed7b3a78bb7d395de34cfe20809e
lee <248197941@qq.com>
2022-11-03 09:04:45 +0800

    change method of KMP match

--- algorithm/KMP/index.js
+++ algorithm/KMP/index.js
@@ -7,36 +7,56 @@
 /**
  * next 数组求解的核心算法
  */
+// const match = function (s) {
+//   /**
+//    * 该函数是求解字符串s中的每一个的字符的公共前后缀的长度
+//    * KMP算法就是根据公共前后缀长度来决定每次移动的距离
+//    */
+//   let next = new Array(s.length).fill(0)
+//   for (let i = 0, j = 1; j < s.length; j++) {
+//     /**
+//      * 单个字符没有公共前后缀，因此 next[0] 等于 0
+//      * 从第二个字符开始往前匹配，只要匹配成功就让 i,j 都加 1
+//      * 当无法匹配时，改变i的值，继续往前寻找匹配字符，如果没有找到
+//      * 将当前字符的公共前后缀设置为0
+//      */
+//     if (s[i] == s[j]) {
+//       next[j] = i + 1
+//       j++
+//       i++
+//     } else {
+//       // 当i等于0时说明当前字符的公共前后缀为0
+//       if (i == 0) {
+//         next[j] = next[0]
+//         j++
+//       }
+//       /**
+//        * 当出现不相等的情况，将i值设置为当前比较字符的
+//        * 前一位字符的公共前后缀长度直到 i= 0为止
+//        */
+//       i = next[i - 1]
+//     }
+//   }
+//   return next
+// }
 const match = function (s) {
-  /**
-   * 该函数是求解字符串s中的每一个的字符的公共前后缀的长度
-   * KMP算法就是根据公共前后缀长度来决定每次移动的距离
-   */
-  let next = new Array(s.length).fill(0)
-  for (let i = 0, j = 1; j < s.length; j++) {
-    /**
-     * 单个字符没有公共前后缀，因此 next[0] 等于 0
-     * 从第二个字符开始往前匹配，只要匹配成功就让 i,j 都加 1
-     * 当无法匹配时，改变i的值，继续往前寻找匹配字符，如果没有找到
-     * 将当前字符的公共前后缀设置为0
-     */
-    if (s[i] == s[j]) {
-      next[j] = i + 1
-      j++
-      i++
-    } else {
-      // 当i等于0时说明当前字符的公共前后缀为0
-      if (i == 0) {
-        next[j] = next[0]
-        j++
-      }
-      /**
-       * 当出现不相等的情况，将i值设置为当前比较字符的
-       * 前一位字符的公共前后缀长度直到 i= 0为止
-       */
-      i = next[i - 1]
+  // 创建一个长度为s的数组用于保存每个字符的最长公共部分，并初始化为0
+  const next = new Array(s.length).fill(0)
+  // next数组已经初始化并且第一个字符始最长公共部分始终为0
+  for (let i = 1; i < s.length; i++) {
+    // 这里是去比较是s[next[i]-1]当前s[i]的最长公共长度的下一位是否相等
+    let j = next[i - 1]
+    while (s[i] !== s[j] && j >= 0) {
+      if (j == 0) break
+      j = next[j] - 1
     }
+    // 如果s[i]等于s[j],那么当前s[i]的最长公共部分的长度为j+1
+    if (s[i] == s[j]) {
+      next[i] = j + 1
+    }
+
   }
+
   return next
 }
 
@@ -49,7 +69,7 @@ const KMP = function (s1, s2) {
   // 先求出模板串的next数组
   let next = match(s1)
   let i = 0
-  for (let j = 0; j < s2.length; ) {
+  for (let j = 0; j < s2.length;) {
     // 当i等于模板串的长度时，表示找到了子串
     if (s1[i] == s2[j] && i == s1.length - 1) return true
     if (s1[i] !== s[j]) {
